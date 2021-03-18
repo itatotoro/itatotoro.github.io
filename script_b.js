379f578766a7a49fd4acd5dfb457f6de5f7c64fd
Glitch (it-a-totoro) <none>
2021-03-18 10:50:36 +0000

    ✂️🕷 bdayting

---
+++ script_b.js
@@ -0,0 +1,14 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+function appear() {
+  document.getElementById("poem").style.opacity = 1;
+}
+
+function ap(obj){
+  obj.style.opacity = 1;
+}
+
+function ds(obj){
+  obj.style.opacity = 0;
+}
